546d92ceba94fe8f48f5db8d81498f74cac39d36
xtareq <xtrinsic96@gmail.com>
2021-09-01 20:02:43 +0600

    add pdf and image

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,10 @@
-export declare function whoami(name: string): string;
+export declare class Invoicer {
+    html: any;
+    data: any;
+    page: any;
+    browser: any;
+    constructor(htmlTemplate: string, data: any);
+    toImage(path: string, options?: {}): Promise<string>;
+    toPdf(path: string): Promise<string>;
+}
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
-export function whoami(name:string) {
-    return "you\'re " + name;
-}
+import Mustache from 'mustache'
+import puppeteer  from 'puppeteer'
+import fs from 'fs'
 
-export function whatDateIsToday() {
-    return new Date().toDateString();
+
+
+export class Invoicer{
+    html: any
+    data: any
+    page: any
+    browser: any
+    constructor(htmlTemplate:string, data:any) {
+        this.html = htmlTemplate
+        this.data = data
+    }
+
+    async toImage(path: string, options={}) {
+        let data = Mustache.render(this.html, this.data);
+        const browser = await puppeteer.launch({
+                headless: true,
+                // pipe: true, <-- delete this property
+                args: [
+                    '--no-sandbox',
+                    '--disable-dev-shm-usage', // <-- add this one
+                    ],
+            });
+        const page = await browser.newPage();
+        await page.setContent(data)
+        await page.screenshot({ path: path, ...options });
+        await browser.close()
+        return "created"
+    }
+
+    async toPdf(path: string) {
+            let data = Mustache.render(this.html, this.data);
+             const browser = await puppeteer.launch({
+                headless: true,
+                // pipe: true, <-- delete this property
+                args: [
+                    '--no-sandbox',
+                    '--disable-dev-shm-usage', // <-- add this one
+                    ],
+            })
+            const page = await browser.newPage();
+            await page.setContent(data)
+            await page.pdf({ path: path, format: 'a4' });
+            await browser.close()
+            return "created"
+    }
 }
